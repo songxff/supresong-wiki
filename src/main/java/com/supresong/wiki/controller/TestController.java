082dc109f497f47b31ec69dbf2da11d88d670888
supresong <supresong@qq.com>
2023-02-16 21:23:46 +0800

    增加自定义配置项实例 0.0.9

--- src/main/java/com/supresong/wiki/controller/TestController.java
+++ src/main/java/com/supresong/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.supresong.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")//冒号后面是默认值,以防配置信息未传进来,导致项目启动失败.
+    private String testHello;
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!!!";
+        return "Hello World!!!"+ testHello;
     }
 
     @PostMapping("/hello/post")
