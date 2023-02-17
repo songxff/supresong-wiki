d8f8e0a747f70cb192e98d4b256dafce14118b4b
supresong <supresong@qq.com>
2023-02-17 13:50:36 +0800

    制作copyUtil封装BeanUtils 0.0.21

--- src/main/java/com/supresong/wiki/service/EbookService.java
+++ src/main/java/com/supresong/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.supresong.wiki.domain.EbookExample;
 import com.supresong.wiki.mapper.EbookMapper;
 import com.supresong.wiki.req.EbookReq;
 import com.supresong.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.supresong.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,15 +22,21 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        //创建一个返回对象的实例
+/*        //创建一个返回对象的实例
         List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList) {
             //在原有查询到的对象里进行遍历,再赋值给返回对象
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
+            //EbookResp ebookResp = new EbookResp();
+            //BeanUtils.copyProperties(ebook,ebookResp);
+
+            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+
             respList.add(ebookResp);
-        }
+        }*/
+
+        List<EbookResp> respkList = CopyUtil.copyList(ebookList, EbookResp.class);
+
         //进行返回经过构建后的返回值
-        return respList;
+        return respkList;
     }
 }
