3f0696169cfbc84f000ecd2a550f93228f1977ca
supresong <supresong@qq.com>
2023-02-18 16:35:38 +0800

    电子书布局调整 0.0.32 一行显示三列电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -45,44 +45,30 @@
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
-      <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-      >
-        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-          <template #footer>
-            <div>
-              <b>ant design vue</b>
-              footer part
-            </div>
-          </template>
-          <template #renderItem="{ item }">
-            <a-list-item key="item.title">
-              <template #actions>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
             <component v-bind:is="type" style="margin-right: 8px"/>
             {{ text }}
           </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #extra>
-                <img
-                    width="272"
-                    alt="logo"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                />
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
               </template>
-              <a-list-item-meta :description="item.description">
-                <template #title>
-                  <a :href="item.href">{{ item.title }}</a>
-                </template>
-                <template #avatar>
-                  <a-avatar :src="item.avatar"/>
-                </template>
-              </a-list-item-meta>
-              {{ item.content }}
-            </a-list-item>
-          </template>
-        </a-list>
-      </a-layout-content>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
+    </a-layout-content>
   </a-layout>
 </template>
 
@@ -149,7 +135,7 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.undefined-has-sider{
+.undefined-has-sider {
   display: flex;
   flex-direction: row;
 }
