cb816ebc250d4509dec55c838637abc70f12f2d1
supresong <supresong@qq.com>
2023-02-18 19:27:02 +0800

    增加AOP，打印接口耗时，请求参数和返回参数 0.0.41 去除过滤器,拦截器

--- src/main/java/com/supresong/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/supresong/wiki/config/SpringMvcConfig.java
@@ -1,38 +1,38 @@
-package com.supresong.wiki.config;
-
-
-import com.supresong.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    /**
-     * 将日志拦截器注入
-     * @param registry
-     */
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                //针对所有的请求
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/login"
-                        //"/test/**",
-                        //"/redis/**",
-                        //"/user/login",
-                        //"/category/all",
-//                        "/ebook/list",
-//                        "/doc/all/**",
-//                        "/doc/vote/**",
-//                        "/doc/find-content/**",
-//                        "/ebook-snapshot/**"
-                );
-    }
-}
+//package com.supresong.wiki.config;
+//
+//
+//import com.supresong.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    /**
+//     * 将日志拦截器注入
+//     * @param registry
+//     */
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                //针对所有的请求
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/login"
+//                        //"/test/**",
+//                        //"/redis/**",
+//                        //"/user/login",
+//                        //"/category/all",
+////                        "/ebook/list",
+////                        "/doc/all/**",
+////                        "/doc/vote/**",
+////                        "/doc/find-content/**",
+////                        "/ebook-snapshot/**"
+//                );
+//    }
+//}
