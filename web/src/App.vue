09a11b80453a8247dd2a7da57ee3fdf12718cef4
supresong <supresong@qq.com>
2023-02-18 16:18:12 +0800

    加入Ant Design Vue组件 0.0.30

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <a-layout>
+  <a-layout id="components-layout-demo-top-side-2">
     <the-header></the-header>
     <router-view/>
-    <!--    //这个是在组件文件本身中声明的名字-->
     <the-footer></the-footer>
   </a-layout>
 </template>
@@ -21,21 +20,3 @@ export default defineComponent({
 });
 </script>
 
-<style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
-</style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -10,7 +10,7 @@
         <a-sub-menu key="sub1">
           <template #title>
               <span>
-                <user-outlined />
+                <user-outlined/>
                 subnav 1
               </span>
           </template>
@@ -22,7 +22,7 @@
         <a-sub-menu key="sub2">
           <template #title>
               <span>
-                <laptop-outlined />
+                <laptop-outlined/>
                 subnav 2
               </span>
           </template>
@@ -34,7 +34,7 @@
         <a-sub-menu key="sub3">
           <template #title>
               <span>
-                <notification-outlined />
+                <notification-outlined/>
                 subnav 3
               </span>
           </template>
@@ -45,27 +45,89 @@
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
-    <a-layout-content
-        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-    >
-      <pre>{{ebooks}}{{books2}}</pre>
-    </a-layout-content>
+      <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+          <template #footer>
+            <div>
+              <b>ant design vue</b>
+              footer part
+            </div>
+          </template>
+          <template #renderItem="{ item }">
+            <a-list-item key="item.title">
+              <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+              </template>
+              <template #extra>
+                <img
+                    width="272"
+                    alt="logo"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                />
+              </template>
+              <a-list-item-meta :description="item.description">
+                <template #title>
+                  <a :href="item.href">{{ item.title }}</a>
+                </template>
+                <template #avatar>
+                  <a-avatar :src="item.avatar"/>
+                </template>
+              </a-list-item-meta>
+              {{ item.content }}
+            </a-list-item>
+          </template>
+        </a-list>
+      </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios'
+import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
 
+const listData: Record<string, string>[] = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
+  },
   setup() {
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      {type: 'StarOutlined', text: '156'},
+      {type: 'LikeOutlined', text: '156'},
+      {type: 'MessageOutlined', text: '2'},
+    ];
     console.log("setup");
     const ebooks = ref();
-    const ebooks1 = reactive({books:[]});
+    const ebooks1 = reactive({books: []});
 
-    onMounted(() =>{
+    onMounted(() => {
       console.log("onMount");
       axios.get("http://localhost:8880/ebook/list?name=Spring").then(function (response) {
         const data = response.data;
@@ -73,11 +135,22 @@ export default defineComponent({
         ebooks1.books = data.content;
       });
     });
-    return{
+    return {
       ebooks,
-      books2: toRef(ebooks1, "books")
-    }
+      ebooks2: toRef(ebooks1, "books"),
+      listData,
+      pagination,
+      actions,
+    };
+
   }
 
 });
 </script>
+
+<style scoped>
+.undefined-has-sider{
+  display: flex;
+  flex-direction: row;
+}
+</style>
