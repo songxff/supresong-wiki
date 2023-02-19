aa825c36a2514f994c235d4614ea0d12a2de94d5
supresong <supresong@qq.com>
2023-02-19 14:47:59 +0800

    为保存接口增加参数校验 0.0.56

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -162,15 +162,17 @@ export default defineComponent({
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/ebook/save", ebook.value).then((response) => {
+        modalLoading.value = false;
         const data = response.data;
         if (data.success) {
           modalVisible.value = false;
-          modalLoading.value = false;
           //重新加载当前页码
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize,
           });
+        }else{
+          message.error(data.message);
         }
       });
 
