bcfa0374b069df93675fc6ace5943dd48b752bd4
supresong <supresong@qq.com>
2023-02-20 22:40:46 +0800

    0.1.6 前端显示登录昵称，遗留问题，刷新会没有

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -22,7 +22,10 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{user.name}}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
         <span>登录</span>
       </a>
     </a-menu>
@@ -56,9 +59,13 @@ declare let KEY: any;
 export default defineComponent({
   name: 'the-header',
   setup () {
+    // 登录后保存
+    const user = ref();
+    user.value = {};
+    // 用来登录
     const loginUser = ref({
       loginName: "test",
-      password: "test"
+      password: "test123"
     });
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
@@ -76,6 +83,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
+          user.value = data.content;
         } else {
           message.error(data.message);
         }
@@ -86,7 +94,8 @@ export default defineComponent({
       loginModalLoading,
       showLoginModal,
       loginUser,
-      login
+      login,
+      user
     }
   }
 });
