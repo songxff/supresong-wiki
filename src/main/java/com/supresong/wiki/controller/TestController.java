5932417f9e96e1340e923bd7a8666da39df80e2b
supresong <supresong@qq.com>
2023-02-20 22:26:21 +0800

    0.1.5 增加redis测试接口

--- src/main/java/com/supresong/wiki/controller/TestController.java
+++ src/main/java/com/supresong/wiki/controller/TestController.java
@@ -2,19 +2,28 @@ package com.supresong.wiki.controller;
 
 import com.supresong.wiki.domain.Test;
 import com.supresong.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
     @Autowired
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @Value("${test.hello:TEST}")//冒号后面是默认值,以防配置信息未传进来,导致项目启动失败.
     private String testHello;
 
@@ -32,4 +41,17 @@ public class TestController {
     public List<Test> list() {
         return testService.list();
     }
-}
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
+    }}
