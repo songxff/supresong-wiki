428761c3d78c97f1251e073bf63ee8f5090b6e3e
supresong <supresong@qq.com>
2023-02-20 20:44:11 +0800

    0.0.98 密码加密传输和加密存储,双层加密

--- src/main/java/com/supresong/wiki/controller/UserController.java
+++ src/main/java/com/supresong/wiki/controller/UserController.java
@@ -3,10 +3,11 @@ package com.supresong.wiki.controller;
 import com.supresong.wiki.req.UserQueryReq;
 import com.supresong.wiki.req.UserSaveReq;
 import com.supresong.wiki.resp.CommonResp;
-import com.supresong.wiki.resp.UserQueryResp;
 import com.supresong.wiki.resp.PageResp;
+import com.supresong.wiki.resp.UserQueryResp;
 import com.supresong.wiki.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -28,6 +29,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
