b600054562c62b9fb5aa2fff4f66c2241a13078c
supresong <supresong@qq.com>
2023-02-21 21:47:31 +0800

    0.1.31 将欢迎文字提取成组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>欢迎使用supresong's知识库</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+
+export default defineComponent({
+  name: 'the-welcome',
+  setup () {
+
+    return {
+    }
+  }
+});
+</script>
+
--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -26,7 +26,7 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎使用supresong's知识库</h1>
+        <the-welcome></the-welcome>
       </div>
       <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
@@ -67,6 +67,7 @@ import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
 import {Tool} from "@/util/tool";
+import TheWelcome from '@/components/the-welcome.vue';
 import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
 
 // const listData: Record<string, string>[] = [];
@@ -84,6 +85,9 @@ import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue
 
 export default defineComponent({
   name: 'Home',
+  components:{
+    TheWelcome,
+  },
   setup() {
     const ebooks = ref()
     // const actions: Record<string, string>[] = [
