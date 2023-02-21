dc1a5f660f4d7e4e7dcc250b8ffc5aad7dfbf2fb
supresong <supresong@qq.com>
2023-02-21 17:29:55 +0800

    0.1.15  文档页面显示文档阅读数点赞数

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -1,5 +1,4 @@
 <template>
-
   <a-layout>
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
@@ -7,7 +6,7 @@
       <a-row :gutter="24">
         <a-col :span="8">
           <p>
-            <a-form :model="param" layout="inline">
+            <a-form layout="inline" :model="param">
               <a-form-item>
                 <a-button type="primary" @click="handleQuery()">
                   查询
@@ -21,28 +20,28 @@
             </a-form>
           </p>
           <a-table
+              v-if="level1.length > 0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :loading="loading"
               :pagination="false"
-              v-if="level1.length > 0"
-              :default-expand-all-rows="true"
               size="small"
+              :defaultExpandAllRows="true"
           >
             <template #name="{ text, record }">
               {{record.sort}} {{text}}
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)"  size="small">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
-                    title="删除后不可恢复，确认删除？"
+                    title="删除后不可恢复，确认删除?"
                     ok-text="是"
                     cancel-text="否"
-                    @confirm="showConfirm(record.id)"
+                    @confirm="handleDelete(record.id)"
                 >
                   <a-button type="danger" size="small">
                     删除
@@ -64,7 +63,7 @@
           </p>
           <a-form :model="doc" layout="vertical">
             <a-form-item>
-              <a-input v-model:value="doc.name" placeholder="请输入名称"/>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
             <a-form-item>
               <a-tree-select
@@ -78,8 +77,8 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item >
-              <a-input v-model:value="doc.sort" placeholder="请输入顺序"/>
+            <a-form-item>
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
             <a-form-item>
               <a-button type="primary" @click="handlePreviewContent()">
@@ -92,38 +91,52 @@
           </a-form>
         </a-col>
       </a-row>
+
       <a-drawer width="900" placement="right" :closable="false" :visible="drawerVisible" @close="onDrawerClose">
         <div class="wangeditor" :innerHTML="previewHtml"></div>
       </a-drawer>
 
     </a-layout-content>
   </a-layout>
+
+  <!--<a-modal-->
+  <!--  title="文档表单"-->
+  <!--  v-model:visible="modalVisible"-->
+  <!--  :confirm-loading="modalLoading"-->
+  <!--  @ok="handleModalOk"-->
+  <!--&gt;-->
+  <!--  -->
+  <!--</a-modal>-->
 </template>
 
-
 <script lang="ts">
-import {defineComponent, onMounted, ref, createVNode} from 'vue';
+import { defineComponent, onMounted, ref, createVNode } from 'vue';
 import axios from 'axios';
-import {message} from 'ant-design-vue';
+import {message, Modal} from 'ant-design-vue';
 import {Tool} from "@/util/tool";
 import {useRoute} from "vue-router";
-import { Modal } from 'ant-design-vue';
-import { ExclamationCircleOutlined } from '@ant-design/icons-vue';
-import E from 'wangeditor';
+import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
+import E from 'wangeditor'
 
 export default defineComponent({
-  name: 'Doc',
+  name: 'AdminDoc',
   setup() {
-    const editor = new E('#content')
     const route = useRoute();
+    console.log("路由：", route);
+    console.log("route.path：", route.path);
+    console.log("route.query：", route.query);
+    console.log("route.param：", route.params);
+    console.log("route.fullPath：", route.fullPath);
+    console.log("route.name：", route.name);
+    console.log("route.meta：", route.meta);
     const param = ref();
     param.value = {};
     const docs = ref();
-    // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
-    const treeSelectData = ref()
-    treeSelectData.value = []
     const loading = ref(false);
-    editor.config.zIndex = 0
+    // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
+    const treeSelectData = ref();
+    treeSelectData.value = [];
+
     const columns = [
       {
         title: '名称',
@@ -131,9 +144,9 @@ export default defineComponent({
         slots: { customRender: 'name' }
       },
       {
-        title: '操作',
+        title: 'Action',
         key: 'action',
-        slots: {customRender: 'action'}
+        slots: { customRender: 'action' }
       }
     ];
 
@@ -149,7 +162,8 @@ export default defineComponent({
      * }]
      */
     const level1 = ref(); // 一级文档树，children属性就是二级文档
-    level1.value = []; //查询初始时把内容框清空
+    level1.value = [];
+
     /**
      * 数据查询
      **/
@@ -157,175 +171,193 @@ export default defineComponent({
       loading.value = true;
       // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
       level1.value = [];
-      axios.get("/doc/all/"+route.query.ebookId).then((response) => {
+      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
         loading.value = false;
         const data = response.data;
         if (data.success) {
           docs.value = data.content;
+          console.log("原始数组：", docs.value);
+
           level1.value = [];
           level1.value = Tool.array2Tree(docs.value, 0);
+          console.log("树形结构：", level1);
+
           // 父文档下拉框初始化，相当于点击新增
           treeSelectData.value = Tool.copy(level1.value);
           // 为选择树添加一个"无"
           treeSelectData.value.unshift({id: 0, name: '无'});
         } else {
-          message.error(data.message)
+          message.error(data.message);
         }
-
       });
-    }
-
+    };
 
     // -------- 表单 ---------
-
     const doc = ref();
     doc.value = {
       ebookId: route.query.ebookId
     };
-
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const editor = new E('#content');
+    editor.config.zIndex = 0;
 
     const handleSave = () => {
-
+      modalLoading.value = true;
       doc.value.content = editor.txt.html();
       axios.post("/doc/save", doc.value).then((response) => {
-
-        const data = response.data;
+        modalLoading.value = false;
+        const data = response.data; // data = commonResp
         if (data.success) {
-          //重新加载当前页码
+          // modalVisible.value = false;
+          message.success("保存成功！");
+
+          // 重新加载列表
           handleQuery();
-          message.success("保存成功！")
-        }else{
+        } else {
           message.error(data.message);
         }
       });
-
     };
+
     /**
      * 将某节点及其子孙节点全部置为disabled
      */
     const setDisable = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
       for (let i = 0; i < treeSelectData.length; i++) {
-        const node = treeSelectData[i]
+        const node = treeSelectData[i];
         if (node.id === id) {
-          node.disabled = true
-          const children = node.children
+          // 如果当前节点就是目标节点
+          console.log("disabled", node);
+          // 将目标节点设置为disabled
+          node.disabled = true;
+
+          // 遍历所有子节点，将所有子节点全部都加上disabled
+          const children = node.children;
           if (Tool.isNotEmpty(children)) {
             for (let j = 0; j < children.length; j++) {
               setDisable(children, children[j].id)
             }
           }
         } else {
-          const children = node.children
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
           if (Tool.isNotEmpty(children)) {
-            setDisable(children, id)
+            setDisable(children, id);
           }
         }
       }
     };
 
+    const deleteIds: Array<string> = [];
+    const deleteNames: Array<string> = [];
     /**
      * 查找整根树枝
      */
-    let ids: Array<string> = []
-    let idsName: Array<string> = []
     const getDeleteIds = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
       for (let i = 0; i < treeSelectData.length; i++) {
-        const node = treeSelectData[i]
+        const node = treeSelectData[i];
         if (node.id === id) {
-          ids.push(node.id)
-          idsName.push(node.name)
+          // 如果当前节点就是目标节点
+          console.log("delete", node);
+          // 将目标ID放入结果集ids
+          // node.disabled = true;
+          deleteIds.push(id);
+          deleteNames.push(node.name);
 
-          const children = node.children
+          // 遍历所有子节点
+          const children = node.children;
           if (Tool.isNotEmpty(children)) {
             for (let j = 0; j < children.length; j++) {
               getDeleteIds(children, children[j].id)
             }
           }
         } else {
-          const children = node.children
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
           if (Tool.isNotEmpty(children)) {
-            getDeleteIds(children, id)
+            getDeleteIds(children, id);
           }
         }
       }
     };
 
-
     /**
-     * 查询富文本内容
+     * 内容查询
      **/
     const handleQueryContent = () => {
-      axios.get("/doc/find-content/" + doc.value.id).then((resp) => {
-        const data = resp.data;
+      axios.get("/doc/find-content/" + doc.value.id).then((response) => {
+        const data = response.data;
         if (data.success) {
-          // 渲染富文本内容
           editor.txt.html(data.content)
         } else {
           message.error(data.message);
         }
-      })
-    }
-
-
-
+      });
+    };
 
     /**
      * 编辑
      */
     const edit = (record: any) => {
-      // 清空富文本内容
-      editor.txt.html("")
-
+      // 清空富文本框
+      editor.txt.html("");
+      modalVisible.value = true;
       doc.value = Tool.copy(record);
       handleQueryContent();
-      treeSelectData.value = Tool.copy(level1.value)
-      setDisable(treeSelectData.value, record.id)
-      treeSelectData.value.unshift({id: 0, name: '无'})
+
+      // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
+      treeSelectData.value = Tool.copy(level1.value);
+      setDisable(treeSelectData.value, record.id);
+
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
+
     /**
      * 新增
      */
     const add = () => {
-      // 清空富文本内容
-      editor.txt.html("")
-
+      // 清空富文本框
+      editor.txt.html("");
+      modalVisible.value = true;
       doc.value = {
         ebookId: route.query.ebookId
       };
+
       treeSelectData.value = Tool.copy(level1.value);
+
+      // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
     };
-    /**
-     * 删除
-     */
-    const del = (id: string) => {
 
-      // join() 方法将一个数组（或一个类数组对象）的所有元素连接成一个字符串并返回这个字符串
-      axios.delete("/doc/delete/" + ids.join(",")).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          //重新加载当前页码
-          handleQuery();
-        }
-      });
-    };
-    /*
-    * 二次确认弹框*/
-    const showConfirm = (id: string) => {
-      ids = []
-      idsName = []
-      getDeleteIds(level1.value, id)
+    const handleDelete = (id: number) => {
+      // console.log(level1, level1.value, id)
+      // 清空数组，否则多次删除时，数组会一直增加
+      deleteIds.length = 0;
+      deleteNames.length = 0;
+      getDeleteIds(level1.value, id);
       Modal.confirm({
         title: '重要提醒',
         icon: createVNode(ExclamationCircleOutlined),
-        content: '将删除: 【'+ idsName +'】。删除后不可恢复，确认删除？',
+        content: '将删除：【' + deleteNames.join("，") + "】删除后不可恢复，确认删除？",
         onOk() {
-          del(id)
+          // console.log(ids)
+          axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
+            const data = response.data; // data = commonResp
+            if (data.success) {
+              // 重新加载列表
+              handleQuery();
+            }
+          });
         },
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        onCancel() {},
-      })
+      });
     };
+
     // ----------------富文本预览--------------
     const drawerVisible = ref(false);
     const previewHtml = ref();
@@ -338,34 +370,36 @@ export default defineComponent({
       drawerVisible.value = false;
     };
 
-
     onMounted(() => {
-      handleQuery()
-      editor.create()
+      handleQuery();
+
+      editor.create();
     });
+
     return {
-      //表格
       param,
-      //docs,
+      // docs,
       level1,
       columns,
       loading,
       handleQuery,
+
+      edit,
+      add,
+
+      doc,
+      modalVisible,
+      modalLoading,
+      handleSave,
+
+      handleDelete,
+
       treeSelectData,
+
       drawerVisible,
       previewHtml,
       handlePreviewContent,
       onDrawerClose,
-
-      //点击操作
-      edit,
-      add,
-      showConfirm,
-
-      //表单
-      doc,
-      handleSave,
-      del
     }
   }
 });
